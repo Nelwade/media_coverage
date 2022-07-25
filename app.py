1ff7a211accb2c49586463d2cbdf6d77f075d53e
Owade <82964062+Nelwade@users.noreply.github.com>
2022-07-25 22:49:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,7 @@ graphs = html.Div(
             ],
             className="mt-4",
         ),
-        # dbc.Row(
-        #     [
-        #         dbc.Col(dcc.Graph(figure=fig2), lg=6),
-        #     ],
-        #     className="mt-4",
-        # ),
+       
     ]
 )
 
@@ -61,24 +56,6 @@ footer = html.Div([
 ])
 
 app.layout = dbc.Container([title, heading, date, content, graphs, footer], fluid=True)
-
-# app.layout= html.Div([
-#     html.H1("Tracking Digital Media Coverage of Presidential Candidates"),
-#     #html.H5("(Articles are counted from nation.africa and standardmedia.co.ke)"),
-#     html.Em(f"(Last Updated at : {now} EAT)"),
-#     html.Br(),
-#     html.Br(),
-#     html.P("This website tracks, in real-time, the number of articles that mention each of the main presidential candidates and their running mates. The data is collected from nation.africa and standardmedia.co.ke."),
-#     html.P("All data is recorded from 21st July 2022."),
-#     dcc.Graph(id= 'graph', figure=fig1),
-#     html.Br(),
-#     html.Br(),
-#     dcc.Graph(id= 'graph2', figure=fig2),
-#     html.Br(),
-#     html.Br(),
-#     html.Br(),
-#     html.P("For any queries reach out through o.owadenelson@gmail.com.")
-#     ])
 
 if __name__ == "__main__":
     app.run(debug=True, port=8010, host='localhost')
